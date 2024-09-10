05285bce6b28915b9babc060b41a68506d78c854
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-09-10 22:34:52 +0530

    final

--- register.py
+++ register.py
@@ -1,10 +1,19 @@
 # User registration - SignIn/SignUp 
 
 from database import * 
-
+import random
 def SignUp() :
     username=input("Create Username: ") 
     temp=db_query(f"SELECT username FROM customers where username = '{username}';") 
-    print(temp) 
+    if temp :
+        print("Username Already Exists") 
+        SignUp()
+    else :
+        print("Username is Available Please Proceed")  
+        password=input("Enter Your Password ") 
+        name = input("Enter Your Name: ") 
+        age=input("Enter Your age: ") 
+        city=input("Enter your city: ")
+        account_number=random.randint(10000000,99999999)   
 
 SignUp() 
