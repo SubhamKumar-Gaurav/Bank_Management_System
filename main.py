287bf5482725003c2ef8a695d39213fb3d437658
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-09-10 22:15:49 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Welcome to Subham Bank, Paisa hi Paisa hai mere paas !!")
+
+while True :
+    try :
+        register = int(input("1. SignUp\n" 
+                             "2. SignIn"))
+        
+        if register==1 or register==2 :
+            pass 
+        else :
+            print("Please Enter Valid Input From Options")
+    except ValueError :
+        print("Invalid Input. Try Again With Numbers")     
