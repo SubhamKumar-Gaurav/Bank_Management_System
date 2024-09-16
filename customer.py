862d3eb63fd4bf18182d8f3139408d7ac1e71844
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-09-16 16:56:23 +0530

    updated file commit

--- customer.py
+++ customer.py
@@ -1,15 +1,18 @@
-# Customer Details 
+# Customer Details  
+
 from database import * 
 
+# Defining Customer class 
 class Customer :
     def __init__(self,username, password, name, age, city, account_number) :
-        self.__username = username 
+        self.__username = username    # Private attributes 
         self.__password = password 
         self.__name = name 
         self.__age = age 
         self.__city = city 
         self.__account_number = account_number 
     
+    # To create a new customer record in the database
     def createuser(self) : 
-        db_query(f"INSERT INTO customers VALUES ('{self.__username}', '{self.__password}', '{self.__name}', '{self.__age}', '{self.__city}',0, '{self.__account_number}', 1 );") 
+        db_query(f"INSERT INTO customers VALUES ('{self.__username}', '{self.__password}', '{self.__name}', '{self.__age}', '{self.__city}', 0 , '{self.__account_number}', 1 );") 
         mydb.commit() 
